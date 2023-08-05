16e08f7425d94f27bedcca4b30c574cfb444a671
njbeepbeep <njbeepbeep@icloud.com>
2023-08-05 10:39:59 +1000

    MacOS changes

--- MacOS-collection-thingy/Assets/Scripts/Coin.cs
+++ MacOS-collection-thingy/Assets/Scripts/Coin.cs
@@ -1,12 +1,12 @@
+using UnityEngine;
+
 public class Coin : MonoBehaviour
 
 {
     //Keeps track of total coin count in scene
-
     public static int CoinCount = 0;
 
     void Start()
-
     {
         //Object created, increment coin count
         ++Coin.CoinCount;
@@ -15,14 +15,20 @@ public class Coin : MonoBehaviour
     //Called when object is destroyed
 
     void OnDestroy()
-
     {
         --Coin.CoinCount;
 
         if (Coin.CoinCount <= 0)
-
         {
             //We have won
+        }
+    }
+    void OnTriggerEnter(Collider Col)
+    {
+        // Coin object is destroyed when collected
+        if (Col.CompareTag("Player"))
+        {
+            Destroy(gameObject);
         }
     }
 }
